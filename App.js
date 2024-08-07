a04bea19e111270e19b15b081c0ede9f478e45b6
Bharath <bharathahgowda@gmail.com>
2024-08-07 20:27:45 +0530

    Committing changes in DemoReactNative submodule

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text,  View } from 'react-native';
+
+// export default function App() {
+//   const handlePress = () => console.log("Text pressed")
+//   return (
+//     <View style={styles.container}>
+       
+//       <Text numberOfLines={1} onPress={handlePress}>Welcome to android app in react native </Text>
+      
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image,  View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       
+      <Text >Welcome to android app in react native </Text>
+      <Image 
+      source={{
+        width:200,
+        height:300,
+        uri:"https://picsum.photos/200/300"}}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +49,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
